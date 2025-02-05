e1ed21cb7398ca2b877026b1a73a4d9d956d2e40
matk0 <matej@occupationbliss.com>
2025-02-05 08:18:57 +0100

    Upgrade to Rails 8.0.0.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_02_080601) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_05_071338) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
