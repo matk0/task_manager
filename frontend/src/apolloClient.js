cb6f27adb594e24e4886b4260678285c48bc8eaa
matk0 <matej@occupationbliss.com>
2025-02-05 12:17:18 +0100

    Use environment variables for FE.

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,13 +1,13 @@
-import * as ActionCable from "@rails/actioncable";
-import ActionCableLink from "graphql-ruby-client/subscriptions/ActionCableLink";
-import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
-import { getMainDefinition } from "@apollo/client/utilities";
+import * as ActionCable from '@rails/actioncable';
+import ActionCableLink from 'graphql-ruby-client/subscriptions/ActionCableLink';
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
 
-const cable = ActionCable.createConsumer("ws://localhost:3000/cable");
+const cable = ActionCable.createConsumer(import.meta.env.VITE_ACTION_CABLE_URL);
 const actionCableLink = new ActionCableLink({ cable });
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:3000/graphql",
+  uri: import.meta.env.VITE_API_URL,
 });
 
 const splitLink = split(
@@ -15,8 +15,8 @@ const splitLink = split(
     const definition = getMainDefinition(query);
 
     return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
     );
   },
   actionCableLink,
